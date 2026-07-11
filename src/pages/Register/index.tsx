1cccca90f8b9c41e03df8de4cd3243f04ca44a65
ryanoliveir <ryandehallo229@gmail.com>
2026-07-11 12:33:22 -0300

    chore: use components on register page

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,50 +1,27 @@
 import { Link } from "react-router-dom";
+import Label from "../../components/Label";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
 
 function RegisterPage() {
   return (
     <div className="flex flex-col gap-5 h-full justify-center max-w-sm mx-auto font-medi">
       <h1 className="text-heading leading-8 font-bold">Criar conta</h1>
       <div className="flex flex-col gap-1">
-        <label
-          className="text-label-medium font-medium leading-5.5 "
-          htmlFor=""
-        >
-          Nome
-        </label>
-        <input
-          className="bg-background-secondary  border border-border-primary text-medium h-10 p-3 rounded-lg placeholder:text-content-placeholder placeholder:text-medium hover:border-content-heading focus:border-accent-brand outline-none text-content-body"
-          placeholder="Digite seu e-mail"
-        />
+        <Label>Nome</Label>
+        <Input type="text" placeholder="Digite seu nome." />
       </div>
       <div className="flex flex-col gap-1">
-        <label className="text-label-medium leading-5.5 font-medium" htmlFor="">
-          E-mail
-        </label>
-        <input
-          className="bg-background-secondary border border-border-primary text-medium h-10 p-3 rounded-lg placeholder:text-content-placeholder placeholder:text-medium hover:border-content-heading focus:border-accent-brand outline-none text-content-body"
-          type="email"
-          placeholder="Digite seu e-mail"
-        />
+        <Label>E-mail</Label>
+        <Input type="text" placeholder="Digite seu e-mail" />
       </div>
       <div className="flex flex-col gap-1">
-        <label className="text-label-medium leading-5.5 font-medium">
-          Senha
-        </label>
-        <input
-          className="bg-background-secondary border  border-border-primary h-10 p-3 rounded-lg placeholder:text-content-placeholder placeholder:text-medium hover:border-content-heading focus:border-accent-brand outline-none text-content-body"
-          type="password"
-          placeholder="Insira sua senha"
-        />
+        <Label>Senha</Label>
+        <Input type="password" placeholder="Insira sua senha" />
       </div>
       <div className="flex flex-col gap-1">
-        <label className="text-label-medium leading-5.5 font-medium">
-          Repetir senha
-        </label>
-        <input
-          className="bg-background-secondary border border-border-primary h-10 p-3 rounded-lg placeholder:text-content-placeholder placeholder:text-medium hover:border-content-heading focus:border-accent-brand outline-none text-content-body"
-          type="password"
-          placeholder="Insira sua senha"
-        />
+        <Label>Repetir senha</Label>
+        <Input type="password" placeholder="Insira novamente sua senha." />
       </div>
 
       <div className="text-medium">
@@ -57,9 +34,7 @@ function RegisterPage() {
           </Link>
         </p>
       </div>
-      <button className="bg-accent-brand text-content-inverse h-11.5 rounded-xl text-label-medium font-bold">
-        Criar conta
-      </button>
+      <Button>Criar conta</Button>
     </div>
   );
 }
